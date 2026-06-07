eaac26fbc52201eec8b025a0d2b47253afc67064
Payman Tohidifar <payman.tohidifar@gmail.com>
2026-06-06 19:30:55 -0700

    update sample code with yapf

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,11 @@
 # tests/test_sample.py
 
+
 def add_numbers(a: int, b: int) -> int:
     return a + b
 
+
 def test_add_numbers_success():
     """Verify core arithmetic maps correctly."""
+
     assert add_numbers(2, 3) == 5
