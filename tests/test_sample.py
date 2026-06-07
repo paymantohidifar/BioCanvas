6f142b2346b351ed5ea39c2ec933cdbd836e9032
Payman Tohidifar <payman.tohidifar@gmail.com>
2026-06-06 20:15:40 -0700

    refactor: remove add_numbers function and associated tests

--- tests/test_sample.py
+++
@@ -1,9 +0,0 @@
-# tests/biocanvas/test_sample.py
-
-from biocanvas.sample import add_numbers
-
-
-def test_add_numbers_success():
-    """Verify core arithmetic maps correctly."""
-
-    assert add_numbers(2, 3) == 5
